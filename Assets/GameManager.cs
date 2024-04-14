21c6461bd72e37992514a7c08c24ead13b720359
DiogoKaster <diogokaster@gmail.com>
2024-04-14 16:19:56 -0300

    comer bolinhas funciona

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,10 +7,12 @@ public class GameManager : MonoBehaviour
     public GameObject leftWarpNode;
     public GameObject rightWarpNode;
 
+    public AudioSource siren;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        siren.Play();
     }
 
     // Update is called once per frame
